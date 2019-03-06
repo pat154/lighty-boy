1cca2bed494d99d77cb1a743304d14650bf01597
Pat <pat.shelley92@gmail.com>
2019-03-06 15:16:20 +1100

    Adding file from raspi for preservation

---
+++ ledclear.py
@@ -0,0 +1,20 @@
+# Simple demo of of the WS2801/SPI-like addressable RGB LED lights.
+import time
+import RPi.GPIO as GPIO
+ 
+# Import the WS2801 module.
+import Adafruit_WS2801
+import Adafruit_GPIO.SPI as SPI
+ 
+ 
+# Configure the count of pixels:
+PIXEL_COUNT = 150
+ 
+# Alternatively specify a hardware SPI connection on /dev/spidev0.0:
+SPI_PORT   = 0
+SPI_DEVICE = 0
+pixels = Adafruit_WS2801.WS2801Pixels(PIXEL_COUNT, spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE), gpio=GPIO)
+
+# Clear all the pixels to turn them off.
+pixels.clear()
+pixels.show()  # Make sure to call show() after changing any pixels!
